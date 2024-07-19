83788bdda241fa101dae7e4ef767bb519ae5d405
NiharikaBS <NIHARIKA@LAPTOP-QLTDCG3P>
2024-07-19 12:27:02 -0400

    latest

--- utils/shellScripting/constants/constants.sh
+++ utils/shellScripting/constants/constants.sh
@@ -2,8 +2,8 @@ BOLD_TXT=$(tput bold)
 NORMAL_TXT=$(tput sgr0)
 OPTIONS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 Q)
 
-STAGING_SERVER_ALIAS=master
-PROD_SERVER_ALIAS=master
+STAGING_SERVER_ALIAS=manager
+PROD_SERVER_ALIAS=manager
 
 NGINX_REPO=niharikabs/nginx
 CLIENT_REPO=niharikabs/client
